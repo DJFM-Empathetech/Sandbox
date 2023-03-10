34ed19381ce59c8e60e02b7a5c07883bf7f952ce
Dylan Dupasquier <dylandupasquier@gmail.com>
2023-03-10 10:59:39 -0800

    dd/fix/make gooder

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from './ui/Button';
 
 export default function App() {
-  const [color, setColor] = useState('#000000');
-
-  const colorRandom = () => {
-    const color = Math.floor(Math.random() * 16777215).toString(16);
-    return `#${color}`;
-  };
-
+  
   return (
     <View style={styles.container}>
       <Text>Woah, so simple!</Text>
       <StatusBar style="auto" />
-      <Button title="test" color={color} onPress={() => setColor(colorRandom)} />
+      <Button title="test" onPress={() => console.log('woah')} />
     </View>
   );
 }
--- ui/Button.jsx
+++ ui/Button.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const Button = ({ title, onPress, color }) => {
+const Button = ({ title, onPress }) => {
+    const [color, setColor] = useState('#000000');
+
+    const colorRandom = () => {
+        const color = Math.floor(Math.random() * 16777215).toString(16);
+        return `#${color}`;
+    };
+
     const buttonStyles = {
         ...styles.button,
         backgroundColor: color,
     };
 
     return (
-        <TouchableOpacity style={buttonStyles} onPress={onPress}>
+        <TouchableOpacity style={buttonStyles} onPress={() => {
+            setColor(colorRandom);
+            onPress()
+        }}>
             <Text style={styles.text}>{title}</Text>
         </TouchableOpacity>
     );
